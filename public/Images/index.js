f963605e1bfadf54d6c785bf43dae8f9644799c5
Nageshwar <nageshpawarpatil@gmail.com>
2026-03-28 13:02:35 +0530

    assets added

---
+++ public/Images/index.js
@@ -0,0 +1,82 @@
+import logo from "./logo.png";
+
+import web from "./web.png";
+import frontend from "./frontend.png";
+import design from "./design.png";
+import backend from "./backend.png";
+import cloud from "./cloud.png";
+import fullStack from "./fullStack.png";
+
+import menu from "./menu.svg";
+import close from "./close.svg";
+import webLink from "./webLink.png";
+
+import html from "./tech/html.png";
+import css from "./tech/css.png";
+import javascript from "./tech/javascript.png";
+import reactjs from "./tech/reactjs.png";
+import formik from "./tech/formik-icon.png";
+import redux from "./tech/redux.png";
+import mui from "./tech/material-ui.png";
+import tailwind from "./tech/tailwind.png";
+import scss from "./tech/scss.png";
+import nodejs from "./tech/nodejs.png";
+import expressjs from "./tech/expressjs.png";
+import mongodb from "./tech/mongodb.png";
+import mongoose from "./tech/mongoose.png";
+import git from "./tech/git.png";
+import github from "./github.png";
+import S3 from "./tech/Amazon-S3.png";
+import EC2 from "./tech/ec2.png";
+
+import vscode from "./tools/vscode.png";
+import ChatGPT from "./tools/ChatGPT.png";
+import chrome from "./tools/chrome.png";
+import mongodbCompass from "./tools/mongodbCompass.png";
+import postman from "./tools/postman.png";
+import ubuntu from "./tools/ubuntu.png";
+import Impress from "./tools/Impress.png";
+
+import e_commerce from "./e-commerce.png";
+import proConnectup from "./proConnectup.jpg";
+import portfolio from "./portfolio.png";
+
+export {
+  logo,
+  menu,
+  close,
+  webLink,
+  web,
+  frontend,
+  design,
+  backend,
+  fullStack,
+  cloud,
+  html,
+  css,
+  javascript,
+  reactjs,
+  formik,
+  redux,
+  mui,
+  tailwind,
+  scss,
+  nodejs,
+  expressjs,
+  mongodb,
+  mongoose,
+  git,
+  github,
+  EC2,
+  S3,
+  vscode,
+  ChatGPT,
+  chrome,
+  mongodbCompass,
+  postman,
+  ubuntu,
+  Impress,
+  e_commerce,
+  proConnectup,
+  portfolio,
+};
---
+++ public/Images/svg/index.js
@@ -0,0 +1,4 @@
+import { ReactComponent as ReactLogo } from "./react.svg";
+
+export { ReactLogo };
+{/* <Close width="32" height="32" /> */}
